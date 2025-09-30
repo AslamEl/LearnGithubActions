c3508183d6a80ffc31aa835ecba106d6d6329cb9
aslam_mhm <mhdroman171@gmail.com>
2025-09-30 19:59:56 +0530

    check_the_pipeline

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="text-center mt-6 text-gray-500 text-sm">
       <p>
-        © {new Date().getFullYear()} MERN Todo App | CI/CD Pipeline Active 🚀
+        || © {new Date().getFullYear()} MERN Todo App | CI/CD Pipeline Active 🚀||
       </p>
     </footer>
   );
